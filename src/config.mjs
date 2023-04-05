5c4ef377eedcc0095d0afd0bcbd02e1c5e40d45f
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-05 11:51:26 -0400

    updating some starting info

--- src/config.mjs
+++ src/config.mjs
@@ -1,13 +1,13 @@
 import defaultImage from './assets/images/default.png';
 
 const CONFIG = {
-  name: 'AstroWind',
+  name: 'SK Cleaning',
 
-  origin: 'https://astrowind.vercel.app',
+  origin: 'https://skclean.net',
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'AstroWind — Free template for create a website with Astro + Tailwind CSS',
+  title: 'SK Cleaning',
   description:
     '🚀 Suitable for Startups, Small Business, Sass Websites, Professional Portfolios, Marketing Websites, Landing Pages & Blogs.',
   defaultImage: defaultImage,
